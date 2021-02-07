5eacd8d1b9dd05fe7cb0361b83ddc5f1f7a1195b
cpanuphan <panuphan@bananacoding.com>
2021-02-07 11:44:12 +0700

    Add user and login/logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+    
+  end
+  
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Login successfully"
+      redirect_to root_path
+    else
+      flash[:alert] = "Invalid email or password"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Logged out"
+  end
+
+end
